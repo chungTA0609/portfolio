9cf35ea4374434c000701279a22ea1222b13f4c2
Chunglnt <chunglnt@vnpost.vn>
2025-05-29 11:14:40 +0700

    fix build

--- components/sections/footer.tsx
+++ components/sections/footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
             <AnimatedSection direction="up">
                 <div className="container mx-auto flex flex-col items-center justify-between gap-4 md:flex-row px-4 sm:px-6 lg:px-8">
                     <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                        © 2024 Lê Nguyễn Thành Chung. All rights reserved.
+                        © 2025 Lê Nguyễn Thành Chung. All rights reserved.
                     </p>
                 </div>
             </AnimatedSection>
